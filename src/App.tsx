d866c896946ce0564a9eb90bd92807eacf13a1e5
saya <saya@saya-mac-book.local>
2022-08-14 16:14:30 +0900

    fetch JSON data

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+
 import './App.css';
 
+export interface StreamerData {
+  userID: string;
+  displayName: string;
+  picture: string;
+  score: number;
+}
+
 function App() {
+
+  const [isItemLoading, setIsItemLoading] = useState<boolean>(true);
+  const [itemDataList, setItemDataList] = useState<any[]>([])
+
+  const getItemsData = async () => {
+    try {
+      const response = await fetch('https://webcdn.17app.co/campaign/pretest/data.json');
+      const json = await response.json();
+      setItemDataList(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      // This code is for testing of loading indicator
+      await wait(2)
+      setIsItemLoading(false);
+    }
+  }
+  
+  useEffect(() => {
+    getItemsData()
+  }, []);
+
+  //This code is for testing of loading indicator
+  function wait(seconds: number) {
+    return new Promise(resolve => {
+      setTimeout(() => { resolve('') }, seconds * 1000);
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {isItemLoading ?
+        <div><p>Loading data...</p></div>
+        : itemDataList.map((item, i) => (
+          <>
+            <p>{item.userID}</p>
+            <p>{item.displayName}</p>
+            <p>{item.picture}</p>
+            <p>{item.score}</p>
+          </>
+        ))
+      }
+    </div >
   );
 }
 
