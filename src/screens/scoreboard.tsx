ae243d7882a30dcb979813d48000a9273afbaac7
saya <saya@saya-mac-book.local>
2022-08-18 21:05:17 +0900

    updated scoreboard screen

--- src/screens/scoreboard.tsx
+++ src/screens/scoreboard.tsx
@@ -21,30 +21,30 @@ function Scoreboard() {
     useEffect(() => {
         getItemsData();
     }, []);
-    
 
-    useEffect(() => {     
+    useEffect(() => {
         const scoreRandomUpdate = [...itemDataList];
-        
-        // randomly select people to update and update the score randomly
-        [...scoreRandomUpdate]
-        .forEach((element) => {
-            const max = 2000;
-            const min = 200;
-            const randomScore = Math.floor(Math.random() * (max - min + 1) + min);
-            //randomly select 30%
-            const shouldUpdate = Math.random() < 0.3;
-            element.score = shouldUpdate ? element.score + randomScore : element.score
-        });
 
+        // randomly select people to update and update the score randomly
+        scoreRandomUpdate
+            .forEach((element) => {
+                const max = 2000;
+                const min = 200;
+                const randomScore = Math.floor(Math.random() * (max - min + 1) + min);
+                //randomly select 30%
+                const shouldUpdate = Math.random() < 0.3;
+                element.score = shouldUpdate ? element.score + randomScore : element.score
+            });
+
+        // expand into a new array for sorting
         [...scoreRandomUpdate]
             // sort array for new index(new ranking)
             .sort((a, b) => (a.score < b.score ? 1 : -1))
             // update new index
-            .forEach((element, index) => {
-                element.newIndex = index;
+            .forEach((element, i) => {
+                element.newIndex = i;
             });
-  
+
         //this will be called every 1 second
         const timer = setInterval(() => {
             setItemDataList(scoreRandomUpdate);
@@ -59,19 +59,16 @@ function Scoreboard() {
     return (
         <Container>
             <ScoreBoard>
-                {isItemLoading &&
-                    <div><p>Loading data...</p></div>}
-                <ul>
-                    {itemDataList.map((item, i) => (
-                        <ItemList
-                            key={i}
-                            newIndex={item.newIndex}
-                            imageLink={item.picture}
-                            updatedScore={item.score}
-                            displayName={item.displayName}
-                            index={i} />
-                    ))}
-                </ul>
+                {isItemLoading && <p>Loading data...</p>}
+                <ul>{itemDataList.map((item, i) => (
+                    <ItemList
+                        key={i}
+                        newIndex={item.newIndex}
+                        imageLink={item.picture}
+                        updatedScore={item.score}
+                        displayName={item.displayName}
+                        index={i} />
+                ))}</ul>
             </ScoreBoard>
         </Container>
     );
